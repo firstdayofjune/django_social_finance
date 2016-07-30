611edfb195783d8bd7211a50255178a57f8eb2ab
firstdayofjune <kimonline@posteo.de>
2016-07-30 18:51:18 +0200

    Added initial files for Vagrant-provisioning and a basic python requirements list

---
+++ Vagrantfile
@@ -0,0 +1,38 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  # The most common configuration options are documented and commented below.
+  # For a complete reference, please see the online documentation at
+  # https://docs.vagrantup.com.
+
+  # Every Vagrant development environment requires a box. You can search for
+  # boxes at https://atlas.hashicorp.com/search.
+  config.vm.box = "ogarcia/arch-net-x64"
+  config.vm.box_url = "https://atlas.hashicorp.com/ogarcia/boxes/arch-net-x64"
+
+  config.vm.host_name = "social-django"
+  config.vm.synced_folder ".", "/home/vagrant"
+
+  # Provider-specific configuration  
+  config.vm.provider "virtualbox" do |vb|
+    # Customize the amount of memory on the VM:
+    vb.memory = "2048"
+  end
+
+  # Create a forwarded port mapping
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  # Forward Django development-server port
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
+  # Forward PostgreSQL port
+  config.vm.network "forwarded_port", guest: 5432, host: 15432
+
+  # Share an additional folder to the guest VM. The first argument is
+  # the path on the host to the actual folder. The second argument is
+  # the path on the guest to mount the folder. And the optional third
+  # argument is a set of non-required options.
+  # config.vm.synced_folder "../data", "/vagrant_data"
+
+  # Enable provisioning with a shell script.
+  config.vm.provision :shell, path: "bootstrap.sh"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# PG Database configuration
+APP_DB_USER=django
+APP_DB_PASS=31CrYPt1C_P4S5w0rD+
+APP_DB_NAME=social_django
+
+PG_VERSION=9.5
+
+
+# Update package-list and upgrade all packages
+yes | pacman -Syu
+
+#######################################################################################
+# 
+#  Install PostgreSQL
+#
+#######################################################################################
+echo "##############################################"
+echo "Installing PostgreSQL..."
+echo ""
+	
+yes | pacman -S postgresql=9.5.3
+
+cat << EOF | su - postgres
+-- initilaize db-cluster
+initdb --locale $LANG -E UTF8 -D '/var/lib/postgres/data'
+EOF
+
+# auto-start service
+systemctl enable postgresql.service
+systemctl start postgresql.service
+
+
+### The following is taken from:
+# https://github.com/jackdb/pg-app-dev-vm/blob/master/Vagrant-setup/bootstrap.sh
+###
+PG_CONF="/var/lib/postgres/data/postgresql.conf"
+PG_HBA="/var/lib/postgres/data/pg_hba.conf"
+
+# Edit postgresql.conf to change listen address to '*':
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+
+# Append to pg_hba.conf to add password auth:
+echo "host    all             all             all                     md5" >> "$PG_HBA"
+
+# Explicitly set default client_encoding
+echo "client_encoding = utf8" >> "$PG_CONF"
+
+# Restart so that all new config is loaded:
+systemctl restart postgresql.service
+
+cat << EOF | su - postgres -c psql
+-- Create the database user:
+CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+
+-- Create the database:
+CREATE DATABASE $APP_DB_NAME WITH OWNER=$APP_DB_USER
+                                  LC_COLLATE='en_US.utf8'
+                                  LC_CTYPE='en_US.utf8'
+                                  ENCODING='UTF8'
+                                  TEMPLATE=template0;
+EOF
+
+echo "Successfully created PostgreSQL dev virtual machine."
+echo "##############################################"
+echo ""
+
+
+#######################################################################################
+# 
+#  Install Python, pip & Django + requirements
+#
+#######################################################################################
+echo "##############################################"
+echo "Installing Python3, pip and django-packages"
+echo ""
+yes | pacman -S python python-pip
+
+pip install -r requirements.pip
+
+echo 'export DJANGO_SECRET_KEY="io&o60txl_-0k=((ap@ykvrbs%n#!wpltaja-jul0pta(d40d4"' >> /etc/profile
+echo 'export POSTGRES_DB_PW=$APP_DB_PASS' >> /etc/profile
+echo "Successfully installed python & django."
+echo "##############################################"
+echo ""
