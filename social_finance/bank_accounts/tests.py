1c6dea6834750acf502d44ace93bcea7811ffb30
firstdayofjune <kimonline@posteo.de>
2016-07-31 10:34:24 +0200

    Added py.test and first failing test

--- social_finance/bank_accounts/tests.py
+++ social_finance/bank_accounts/tests.py
@@ -1,19 +1,36 @@
 import os
+import pytest
 
 from bank_accounts import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 
+@pytest.mark.django_db
 class UserValidationTest(TestCase):
 	fixtures = [
 		os.path.join('bank_accounts', 'fixtures', 'test_admins.json'),
 	]
 
+	@classmethod
+	def setUpClass(cls):
+		super(UserValidationTest, cls).setUpClass()
+
+		cls.User = get_user_model()
+		cls.admin = cls.User.objects.filter(username='Peter')[0]
+
 
 	def test_firstname_does_not_allow_special_characters(self):
-		# [^(!"#\$%'\(\)\*\+\/\\:;<=>\?@\[\]\^_`\{\|\}~)]
-		pass
+		# [^(!"#\$%\(\)\*\+\/\\:;<=>\?@\[\]\^_`\{\|\}~)]
+		with pytest.raises(ValidationError):
+			user_args = {
+				'admin': self.admin,
+				'firstname': 'Inval|d name',
+				'lastname': 'Valid Name',
+			}
+			new_user = models.BankUser(**user_args)
+			new_user.save()
 
 	def test_lastname_does_not_allow_special_characters(self):
 		pass
@@ -34,6 +51,7 @@ class UserValidationTest(TestCase):
 		pass
 
 
+@pytest.mark.django_db
 class UserManipulationTest(TestCase):
 	fixtures = [
 		os.path.join('bank_accounts', 'fixtures', 'test_admins.json'),
@@ -41,14 +59,14 @@ class UserManipulationTest(TestCase):
 
 	@classmethod
 	def setUpClass(cls):
-	    cls.User = get_user_model()
+		super(UserManipulationTest, cls).setUpClass()
+		cls.User = get_user_model()
 
-	    # Fix the clear-text passwords of fixtures
-	    for user in cls.User.objects.all():
-	        user.set_password(user.password)
-	        user.save()
+		# Fix the clear-text passwords of fixtures
+		for user in cls.User.objects.all():
+			user.set_password(user.password)
+			user.save()
 
-	    super(UserManipulationTest, cls).setUpClass()
 
 	def test_user_cannot_be_updated_by_foreign_admin(self):
 		pass
