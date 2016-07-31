d57cd74934fca62ecc544210bf3deaea1455768b
firstdayofjune <kimonline@posteo.de>
2016-07-31 14:14:48 +0200

    - BankAccount model added - Basic validation on IBAN-field added

--- social_finance/bank_accounts/models.py
+++ social_finance/bank_accounts/models.py
@@ -23,3 +23,22 @@ class BankUser(models.Model):
 	def save(self, *args, **kwargs):
 		self.clean_fields()
 		super(BankUser, self).save(*args, **kwargs)
+
+
+class BankAccount(models.Model):
+	no_special_characters_regex = r'^.*?[(!"#\$%\(\)\*\+\/\\:;<=>\?@\[\]\^_`\{\|\}~)]+.*?$'
+	message = _('Special characters are forbidden')
+	code = 'invalid'
+	no_special_characters = RegexValidator(
+		no_special_characters_regex,
+		message,
+		code,
+		inverse_match=True,
+	)
+
+	holder = models.ForeignKey(BankUser)
+	iban = models.CharField(max_length=34, validators=[no_special_characters])
+
+	def save(self, *args, **kwargs):
+		self.clean_fields()
+		super(BankAccount, self).save(*args, **kwargs)
