d9e8bc40b703591d1c9208758f9a24d28ceb2f5e
firstdayofjune <kimonline@posteo.de>
2016-07-31 13:42:16 +0200

    First BankUser model plus validation

--- social_finance/bank_accounts/models.py
+++ social_finance/bank_accounts/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
+from django.core.validators import RegexValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+class BankUser(models.Model):
+	no_special_characters_regex = r'^.*?[(!"#\$%\(\)\*\+\/\\:;<=>\?@\[\]\^_`\{\|\}~)]+.*?$'
+	message = _('Special characters are forbidden')
+	code = 'invalid'
+	no_special_characters = RegexValidator(
+		no_special_characters_regex,
+		message,
+		code,
+		inverse_match=True,
+	)
+
+	admin = models.ForeignKey(settings.AUTH_USER_MODEL)
+	firstname = models.CharField(max_length=255, validators=[no_special_characters])
+	lastname = models.CharField(max_length=255)
+
+	def save(self, *args, **kwargs):
+		self.clean_fields()
+		super(BankUser, self).save(*args, **kwargs)
