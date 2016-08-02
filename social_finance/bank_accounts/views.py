c2ba978a18ae627a320838a5da166fc8bccf85ac
firstdayofjune <kimonline@posteo.de>
2016-08-02 15:29:47 +0200

    The account_listing now shows the users insted of accounts

--- social_finance/bank_accounts/views.py
+++ social_finance/bank_accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from bank_accounts.models import BankAccount
+from bank_accounts.models import BankUser
 
 # Create your views here.
 def account_listing(request):
-	accounts = BankAccount.objects.all()
-	return render(request, 'bank_accounts/account_listing.html', {'accounts': accounts})	
+	users = BankUser.objects.all()
+	return render(request, 'bank_accounts/account_listing.html', {'users': users})	
