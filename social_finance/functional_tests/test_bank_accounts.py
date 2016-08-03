efcb9b4cfcc2b26288319dd23a74748b4a61016c
firstdayofjune <kimonline@posteo.de>
2016-08-03 11:35:30 +0200

    Added failing test for account update

--- social_finance/functional_tests/test_bank_accounts.py
+++ social_finance/functional_tests/test_bank_accounts.py
@@ -39,6 +39,22 @@ class AccountListingTest(StaticLiveServerTestCase):
 		
 		assert len(accounts) == len(BankUser.objects.first().accounts.all())
 
+
+class UserCRUDTest(StaticLiveServerTestCase):
+	fixtures = [
+		os.path.join('bank_accounts', 'fixtures', 'test_admins.json'),
+		os.path.join('bank_accounts', 'fixtures', 'test_users.json'),
+		os.path.join('bank_accounts', 'fixtures', 'test_accounts.json'),
+	]	
+
+	def setUp(self):
+		self.browser = webdriver.PhantomJS()
+		self.browser.set_window_size(1024, 768)
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
 	def test_if_edit_link_redirects_to_correct_edit_site(self):
 		self.browser.get(self.live_server_url)
 
@@ -72,7 +88,6 @@ class AccountListingTest(StaticLiveServerTestCase):
 		add_slug = reverse('bank-user-create')
 		assert add_user_target.endswith(add_slug)
 
-
 	def test_if_edit_link_site_shows_empty_form(self):
 		self.browser.get(self.live_server_url)
 
@@ -85,3 +100,48 @@ class AccountListingTest(StaticLiveServerTestCase):
 
 		assert firstname_field.get_attribute('value') == ''
 		assert lastname_field.get_attribute('value') == ''
+
+
+class AccountCRUDTest(StaticLiveServerTestCase):
+	fixtures = [
+		os.path.join('bank_accounts', 'fixtures', 'test_admins.json'),
+		os.path.join('bank_accounts', 'fixtures', 'test_users.json'),
+		os.path.join('bank_accounts', 'fixtures', 'test_accounts.json'),
+	]	
+
+	def setUp(self):
+		self.browser = webdriver.PhantomJS()
+		self.browser.set_window_size(1024, 768)
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_if_edit_link_redirects_to_correct_edit_site(self):
+		self.browser.get(self.live_server_url)
+
+		user_panels = self.browser.find_elements_by_class_name('panel-group')
+		account_table = user_panels[0].find_element_by_class_name('table')
+		account_list = account_table.find_element_by_tag_name('tbody')
+		accounts = account_list.find_elements_by_tag_name('tr')
+
+		edit_button = accounts[0].find_element_by_class_name('glyphicon-pencil')
+		edit_button_target = edit_button.get_attribute('href')
+
+		edit_slug = reverse('bank-account-update', kwargs={'pk': BankUser.objects.first().accounts.first().id})
+		assert edit_button_target.endswith(edit_slug)
+
+	def test_if_edit_link_site_shows_prefilled_form(self):
+		self.browser.get(self.live_server_url)
+
+		user_panels = self.browser.find_elements_by_class_name('panel-group')
+		account_table = user_panels[0].find_element_by_class_name('table')
+		account_list = account_table.find_element_by_tag_name('tbody')
+		accounts = account_list.find_elements_by_tag_name('tr')
+
+		edit_button = accounts[0].find_element_by_class_name('glyphicon-pencil')
+		edit_button.click()
+
+		iban_field = self.browser.find_element_by_id('id_iban')
+		
+		assert iban_field.get_attribute('value') == BankUser.objects.first().accounts.objects.first().iban
